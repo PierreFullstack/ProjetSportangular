b708391278020fd0cec1c29c05a8456a4ecac8c9
PierreFullstack <pierre.falck@hotmail.com>
2019-02-28 16:18:22 +0100

    pour l'instant ça marche

---
+++ material.module.ts
@@ -0,0 +1,10 @@
+import {NgModule} from "@angular/core";
+import { CommonModule } from '@angular/common';
+import { MatButtonModule, MatToolbarModule } from '@angular/material';
+
+
+@NgModule({
+imports: [CommonModule, MatButtonModule, MatToolbarModule],
+exports: [CommonModule, MatButtonModule, MatToolbarModule],
+})
+export class CustomMaterialModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatMenuModule, MatSidenavModule } from '@angular/material';
+
+
+import { RouterModule } from '@angular/router';
+
+import { FormsModule } from '@angular/forms';
+
+
 import { AfficheEventComponent } from './affiche-event/affiche-event.component';
+import { ConnexionComponent } from './connexion/connexion.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    AfficheEventComponent
+    AfficheEventComponent,
+    ConnexionComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule,
+    MatButtonModule,
+      MatMenuModule,
+      FormsModule,
+      MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
