4402d9ab2a36598d2ea46051e07a01caf034e553
PierreFullstack <pierre.falck@hotmail.com>
2019-02-28 17:44:33 +0100

    afficher events in progress

--- src/app/material.ts
+++ src/app/material.ts
@@ -1,8 +1,10 @@
 import { MatButtonModule, MatCheckboxModule, MatToolbarModule  } from '@angular/material';
 import { NgModule } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+
 
 @NgModule({
-imports: [MatButtonModule, MatCheckboxModule, MatToolbarModule],
-exports: [MatButtonModule, MatCheckboxModule, MatToolbarModule],
+imports: [MatButtonModule, MatCheckboxModule, MatToolbarModule, MatCardModule],
+exports: [MatButtonModule, MatCheckboxModule, MatToolbarModule, MatCardModule],
 })
 export class MaterialModule { }
